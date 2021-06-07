4152ab05463183c3d7466c45cdfe0e40c2970e5b
ThomasPascual <thoma1@free.fr>
2021-06-07 16:35:38 +0200

    fil rouge pendu exercice 1

---
+++ fil_rouge.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+
+###############PENDU 1################
+//exercice 1
+$mystere = 'mystere';
+$reponse = readline('Entrer un mot: ');
+
+echo $mystere == $reponse ? 'bien joué c\'est le bon mot' : 'raté';
+
+
+//exercice 2
+$list_de_mot = array('mystere', 'toto', 'test', 'aaaaaa');
+$index = array_rand($list_de_mot, 1); //choisit un index parmi la liste
+$mot_a_trouver = $list_de_mot[$index]; //retourne un mot de la liste
+$reponse = readline('Entrer un mot: ');
+echo $mot_a_trouver == $reponse ? 'bien joué c\'est le bon mot' : 'raté';
+
+
+
+
+
+
+
+
