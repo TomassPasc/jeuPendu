ed7d139c25f67d13e314e4872c2b927c371555e6
ThomasPascual <thoma1@free.fr>
2021-06-08 06:35:50 +0200

    les doublons ne font plus perdre de vies

--- fil_rouge.php
+++ fil_rouge.php
@@ -56,15 +56,17 @@ for ($i = 5; $i >=0; $i--){
 $list_de_mot = array('mystere', 'toto', 'test', 'aaaaaa');
 $index = array_rand($list_de_mot, 1); //choisit un index parmi la liste
 $mot_a_trouver = $list_de_mot[$index]; //retourne un mot de la liste
+$rappel = []; //liste des mots et lettre déjà proposé hormis doublons
 
-//$mot_a_trouver = 'aabc';
+$mot_a_trouver = 'aabc';
 
 $result = str_repeat('_ ' , strlen($mot_a_trouver)); //permet d'afficher le motif à trouver ex: _ _ _ _ pour test
 
 
-for ($i = 5; $i >=0; $i--){
+for ($vie = 5; $vie >=0; $vie--){
+    //var_dump($rappel);
     echo $result.PHP_EOL;
-    $i > 1 ? printf('vous avez %d vies'.PHP_EOL, $i) : printf('vous avez %d vie'.PHP_EOL, $i); // affiche et gère le s de vie
+    $vie > 1 ? printf('vous avez %d vies'.PHP_EOL, $vie) : printf('vous avez %d vie'.PHP_EOL, $vie); // affiche et gère le s de vie
     $reponse = readline('Entrer une lettre ou le mot si vous pensez l\'avoir trouver: ');
 
     //l'utilisateur entre une lettre:
@@ -73,22 +75,29 @@ for ($i = 5; $i >=0; $i--){
             echo 'raté';
         }
         else {
+            $doublon = 0;
             for ($c = 0; $c < strlen($mot_a_trouver); $c++){ //on parcourt les lettres du mot à trouver
-                if($mot_a_trouver[$c] === $reponse) { //quand la lettre tapé correspond on la remplace
-                    $result[$c*2] = $reponse; // c*2 correspond à l'indice comme le motif de result est '_ '                  
+                if($mot_a_trouver[$c] === $reponse) { //quand la lettre tapée correspond on la remplace
+                    $result[$c*2] = $reponse; // c*2 correspond à l'indice comme le motif de result est '_ '
+                    $doublon++;                  
                 }
             }  
             if (strpos($result, '_') === false){ // plus de tiret donc le mot est trouvé
                 echo 'gagné';
                 break;
             }
+            if ($doublon > 1) {
+                $vie ++; // permet de ne pas perdre de vie lorsque la lettre se trouve plus d'une fois dans le mot.
+            }
+            
+            
         }
     }
     //l'utilisateur entre un mot:
     else {
         if($mot_a_trouver != $reponse) {
             echo 'raté'.PHP_EOL;
-            if ($i == 0){
+            if ($vie == 0){
                 echo 'perdu';
             }
         }
